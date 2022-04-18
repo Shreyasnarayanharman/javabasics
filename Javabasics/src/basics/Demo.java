7dc28dc356b53a4fe6e3e23123d603b0de90c3d3
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-18 15:53:27 +0530

    Student box

--- Javabasics/src/basics/Demo.java
+++ Javabasics/src/basics/Demo.java
@@ -6,6 +6,16 @@ public class Demo {
 		Student collegeStudent = new Student(); 
 		collegeStudent.name = "Shreyas";
 		System.out.println("Name of the student is "+collegeStudent.name);
-		}
+		collegeStudent.age = 21;
+		System.out.println("Student's age is "+collegeStudent.age);
+		collegeStudent.isMale = true;
+		if(collegeStudent.isMale == true)
+			System.out.println("The gender of the student is Male");
+		else
+			System.out.println("The gender of the student is Female");
+		collegeStudent.address = "Mangalore";
+		System.out.println("The address of the student is "+collegeStudent.address);
+
+	}
 }
 
