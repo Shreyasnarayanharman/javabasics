8c7dd948537667cd1af7591b1e4155bafa8457ff
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-19 14:34:10 +0530

    Inheritance

---
+++ Javabasics/src/basics/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package basics.inheritance;
+
+public class Animal{  
+	void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+	void bark(){System.out.println("barking...");}  
+}  
+class BabyDog extends Dog{  
+	void weep(){System.out.println("weeping...");}  
+}  
