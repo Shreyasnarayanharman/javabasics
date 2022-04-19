71262fa3b3233fcb904b53e221792ec4e59e0629
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-19 16:26:31 +0530

    Debugging

---
+++ Javabasics/src/basics/inheritance/Debugging.java
@@ -0,0 +1,22 @@
+package basics.inheritance;
+
+public class Debugging {
+
+	public static void main(String[] args) {
+		//Debugging debugging = new Debugging();
+		int sum = add(13,14);
+		System.out.println("the result is --"+sum);
+		int summation =0;
+		for(int i=0;i<5;i++) {
+			summation = summation + add(i,i+5);
+		System.out.println(summation);
+
+		}
+	}
+
+	static int add(int a, int b) {
+		int avg = a+b/2;
+		return a+b;
+	}
+
+}
