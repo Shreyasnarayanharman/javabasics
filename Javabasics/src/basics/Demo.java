b8d88d02df31495c6058c47e6b565b4be41c74f3
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-19 11:59:48 +0530

    Constructors

--- Javabasics/src/basics/Demo.java
+++ Javabasics/src/basics/Demo.java
@@ -3,19 +3,24 @@ package basics;
 public class Demo {
 	public static void main(String args[])
 	{
+		
 		Student collegeStudent = new Student(); 
-		collegeStudent.name = "Shreyas";
-		System.out.println("Name of the student is "+collegeStudent.name);
-		collegeStudent.age = 21;
-		System.out.println("Student's age is "+collegeStudent.age);
-		collegeStudent.isMale = true;
-		if(collegeStudent.isMale == true)
-			System.out.println("The gender of the student is Male");
-		else
-			System.out.println("The gender of the student is Female");
-		collegeStudent.address = "Mangalore";
-		System.out.println("The address of the student is "+collegeStudent.address);
+		
+		
+		
+		System.out.println("The name of the Student is "+collegeStudent.name);
+		System.out.println("The age of the Student is "+collegeStudent.age);
+		
+		Student javaStudent = new Student("Shreyas");
+		System.out.println("The name of the Student is "+javaStudent.name);
+		
+		Student cjavaStudent = new Student(22,"Shreyas");
+		System.out.println("The name of the Student is "+cjavaStudent.name);
+		System.out.println("The age of the Student is "+cjavaStudent.age);
+			
+		}
 
 	}
-}
+
+
 
--- Javabasics/src/basics/Student.java
+++ Javabasics/src/basics/Student.java
@@ -6,6 +6,22 @@ public class Student {
 			int age;   
 			boolean isMale;  
 			String address;
-
+			
+			public Student(){
+			 System.out.println("Default Constructor");
+			}		
+			
+			
+			public Student(String sname) {
+				System.out.println("Constructor with 1 parameter");
+				name = sname;
+			}
+			//---------------------------------
+			public Student(int sage,String sname) {
+				System.out.println("Constructor with 2 parameters");
+				name = sname;
+				age = sage;
+			}
+			//-----------------------
 
 }
