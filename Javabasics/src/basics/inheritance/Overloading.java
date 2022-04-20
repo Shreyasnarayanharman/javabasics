9f2c9544d9d15053b344e5497cdfd0bc99d75a00
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-20 10:47:30 +0530

    Method Overloading

---
+++ Javabasics/src/basics/inheritance/Overloading.java
@@ -0,0 +1,9 @@
+package basics.inheritance;
+
+public class Overloading {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Adder.add(11,11));  
+		System.out.println(Adder.add(12.3,12.6)); 
+	}
+}
