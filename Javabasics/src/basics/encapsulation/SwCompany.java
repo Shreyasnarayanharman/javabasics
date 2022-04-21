d269c5c84ac1fac055d5046a1872a80c9ff32a37
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-21 10:11:10 +0530

    try-catch

---
+++ Javabasics/src/basics/encapsulation/SwCompany.java
@@ -0,0 +1,16 @@
+package basics.encapsulation;
+
+
+public class SwCompany {
+
+	public static void main(String[] args) {
+		Programmer programmer = new Programmer();
+		//programmer.name = "Shreyas";
+		programmer.setName("Shreyas"); 
+		programmer.setDob("11/11/11");
+		programmer.age = 14;
+	}
+
+}
+
+
---
+++ Javabasics/src/basics/exception/TryCatchExample.java
@@ -0,0 +1,19 @@
+package basics.exception;
+
+public class TryCatchExample {  
+
+	public static void main(String[] args) {  
+		try  
+		{  
+			int data=50/0; //may throw exception   
+		}  
+		// handling the exception by using Exception class      
+
+		catch(Exception e) { 
+			System.out.println(e); 
+		}
+
+		System.out.println("rest of the code");  
+	}  
+
+}  
