e936397ae35442c6ef6eb13d6538ca09b3019260
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-22 11:57:52 +0530

    Thread Pool

---
+++ Javabasics/src/basics/threads/WorkerThread.java
@@ -0,0 +1,17 @@
+package basics.threads;
+
+class WorkerThread implements Runnable {
+	private String message;
+	public WorkerThread(String s){
+		this.message=s;
+	}
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);
+		processmessage();//call process message method that sleeps the thread for 2 seconds
+		System.out.println(Thread.currentThread().getName()+" (End)");//prints thread name
+	}
+	private void processmessage() {
+		try {  Thread.sleep(2000);  } catch (InterruptedException e) { e.printStackTrace(); }
+	}
+}
