9b46474b05ed8322cd5aad3ef81e90fe1c63c722
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-22 11:02:33 +0530

    thread basics

---
+++ Javabasics/src/basics/threads/FeeThread.java
@@ -0,0 +1,12 @@
+package basics.threads;
+
+
+public class FeeThread extends Thread{
+
+	@Override
+	public void run() {
+		super.run();
+		System.out.println("getting signature from fee dept");
+	}
+
+}
