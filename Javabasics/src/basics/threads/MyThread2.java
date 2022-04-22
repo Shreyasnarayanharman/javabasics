5514b31e8a19125cbd5511c64431e3c8d82c086d
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-22 14:59:27 +0530

    thread synchronisation

---
+++ Javabasics/src/basics/threads/MyThread2.java
@@ -0,0 +1,13 @@
+package basics.threads;
+
+public class MyThread2 extends Thread{  
+	Table t;  
+	MyThread2(Table t, String tName){  
+		super(tName);
+		this.t=t;  
+	}  
+	public void run(){  
+		t.printTable(100);  
+	}  
+} 
+
