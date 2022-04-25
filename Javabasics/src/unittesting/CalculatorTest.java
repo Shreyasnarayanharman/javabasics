a10a3c43e0d8345ecc0c95c733bf63dde7fe184d
SNarayanpg <SNarayanpg@INBAWN166757.ad.harman.com>
2022-04-25 16:19:51 +0530

    junit testing

---
+++ Javabasics/src/unittesting/CalculatorTest.java
@@ -0,0 +1,17 @@
+package unittesting;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalculatorTest {
+
+	@Test
+	void testAdd() {
+		int expected = 40;
+		int actual = Calculator.add(10, 20);
+		assertEquals(expected, actual);
+	}
+}
+
+
---
+++ Javabasics/src/unittesting/DemoCal.java
@@ -0,0 +1,8 @@
+package unittesting;
+
+public class DemoCal {
+	public static void main(String[] args) {
+		System.out.println(Calculator.add(10, 20));
+	}
+}
+
